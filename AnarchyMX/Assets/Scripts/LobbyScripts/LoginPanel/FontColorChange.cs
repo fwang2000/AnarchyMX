0161e61b8bdbe0685791286c1085436625cc082b
fwang2000 <61210395+fwang2000@users.noreply.github.com>
2021-09-26 16:39:56 -0700

    Built Lobby UI System

---
+++ AnarchyMX/Assets/Scripts/LobbyScripts/LoginPanel/FontColorChange.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.EventSystems;
+
+public class FontColorChange : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField]
+    private GameObject loginText;
+    private TextMeshProUGUI loginTextGUI;
+    private bool mouseOver;
+    private Color mouseOverColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        loginTextGUI = loginText.GetComponent<TextMeshProUGUI>();
+        mouseOver = false;
+        mouseOverColor = new Color(1, 0.9098f, 0.6745f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mouseOver)
+        {
+            loginTextGUI.color = mouseOverColor;
+        }
+        else
+        {
+            loginTextGUI.color = Color.white;
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        mouseOver = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        mouseOver = false;
+    }
+}
