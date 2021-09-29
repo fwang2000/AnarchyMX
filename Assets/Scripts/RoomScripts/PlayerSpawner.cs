0a984455ecc4c67b698148bd48bc36d0e81a8c2c
fwang2000 <61210395+fwang2000@users.noreply.github.com>
2021-09-28 21:27:52 -0700

    Began Room Scene Building

---
+++ Assets/Scripts/RoomScripts/PlayerSpawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Pun.UtilityScripts;
+
+public class PlayerSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject spawnpoints;
+
+    private string prefabName = "PlayerPrefab";
+
+    public void InstantiatePlayer(string prefabColor)
+    {
+        PhotonNetwork.Instantiate(prefabColor + prefabName, SpawnpointController.singletonInstance.spawnpoints[PhotonNetwork.LocalPlayer.GetPlayerNumber()].position, Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/RoomScripts/RoomManager.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class RoomManager : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    private GameObject StartButton;
+    private const int MaxPlayersPerRoom = 8;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+        ActivateStartButton();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void ActivateStartButton()
+    {
+        StartButton.SetActive(PhotonNetwork.IsMasterClient);
+    }
+
+    #region PUN Callbacks
+    public override void OnLeftRoom()
+    {
+        SceneManager.LoadScene(SceneManagerHelper.ActiveSceneBuildIndex - 1);
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        if (PhotonNetwork.CurrentRoom.PlayerCount >= MaxPlayersPerRoom - 2)
+        {
+            if (PhotonNetwork.IsMasterClient)
+            {
+                StartButton.GetComponent<Button>().interactable = true;
+            }
+        }
+
+        if (PhotonNetwork.CurrentRoom.PlayerCount == MaxPlayersPerRoom)
+        {
+            PhotonNetwork.CurrentRoom.IsOpen = false;
+            PhotonNetwork.CurrentRoom.IsVisible = false;
+        }
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            if (PhotonNetwork.CurrentRoom.PlayerCount < MaxPlayersPerRoom - 2)
+            {
+                StartButton.GetComponent<Button>().interactable = false;
+            }
+            PhotonNetwork.DestroyPlayerObjects(otherPlayer);
+        }
+    }
+
+    public override void OnMasterClientSwitched(Player newMasterClient)
+    {
+        if (PhotonNetwork.LocalPlayer.ActorNumber == newMasterClient.ActorNumber)
+        {
+            StartButton.SetActive(true);
+        }
+    }
+    #endregion
+
+    public void OnStartButtonClicked()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            PhotonNetwork.LoadLevel("GameScene");
+        }
+    }
+}
